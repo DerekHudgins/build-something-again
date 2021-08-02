167500451f67dfb285972a5f2b2c9e4e01e19266
Derek Hudgins <hudgins.derek@gmail.com>
2021-08-02 16:07:58 -0700

    created test for POST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,22 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  it('creates a book from POST', async () => {
+    const hitch = {
+      title: 'The Hitchhikes Guide To The Galaxy',
+      author: 'Adams',
+      genre: 'Sifi',
+    };
+
+    const res = await request(app).post('/api/v1/books').send(hitch);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...hitch,
+    });
   });
 });
